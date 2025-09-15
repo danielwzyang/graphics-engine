67b4b4a9dc51e05a77eaaf8f80fc5575e09bc978
danielwzyang <danielwzyang@gmail.com>
2025-09-15 16:04:11 -0400

    add makefile for cargo run

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,22 @@ fn main() -> Result<(), Box<dyn Error>> {
        max_color: 255,
    });
 
+   let center = (250.0, 250.0);
+   let rays = 10.0;
+
    for x in 0..500 {
        for y in 0..500 {
-           pic.plot(x, y, (x % 256, y % 256, 255))?;
+           let dx = x as f32 - center.0;
+           let dy = y as f32 - center.1;
+           let dist = (dx*dx + dy*dy).sqrt() / 10.0;
+           let angle = dy.atan2(dx) * rays;
+           let warped = angle * 6.0 + dist.sin() * 3.0;
+
+           let r = (warped.sin() * 127.0 + 128.0) as usize;
+           let g = ((warped + 2.0).sin() * 127.0 + 128.0) as usize;
+           let b = ((warped + 4.0).sin() * 127.0 + 128.0) as usize;
+
+           pic.plot(x, y, (r, g, b))?;
        }
    }
 
