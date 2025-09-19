77bb6e94ae51de64c04a356a92123b7389b21d15
danielwzyang <danielwzyang@gmail.com>
2025-09-19 17:08:35 -0400

    moving draw line to picture

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod picture;
-mod draw;
 mod colors;
 use crate::picture::{Picture};
 use std::error::Error;
@@ -11,26 +10,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     let yres = 500;
 
     // octants 1, 5
-    draw::line(&mut test_pic, 0, 0, xres-1, yres-1, &colors::GREEN)?;
-    draw::line(&mut test_pic, 0, 0, xres-1, yres/2, &colors::GREEN)?;
-    draw::line(&mut test_pic, xres-1, yres-1, 0, yres/2, &colors::GREEN)?;
+    test_pic.draw_line(0, 0, xres-1, yres-1, &colors::GREEN)?;
+    test_pic.draw_line(0, 0, xres-1, yres/2, &colors::GREEN)?;
+    test_pic.draw_line(xres-1, yres-1, 0, yres/2, &colors::GREEN)?;
 
     // octants 8, 4
-    draw::line(&mut test_pic, 0, yres-1, xres-1, 0, &colors::CYAN)?;
-    draw::line(&mut test_pic, 0, yres-1, xres-1, yres/2, &colors::CYAN)?;
-    draw::line(&mut test_pic, xres-1, 0, 0, yres/2, &colors::CYAN)?;
+    test_pic.draw_line(0, yres-1, xres-1, 0, &colors::CYAN)?;
+    test_pic.draw_line(0, yres-1, xres-1, yres/2, &colors::CYAN)?;
+    test_pic.draw_line(xres-1, 0, 0, yres/2, &colors::CYAN)?;
 
     // octants 2, 6
-    draw::line(&mut test_pic, 0, 0, xres/2, yres-1, &colors::RED)?;
-    draw::line(&mut test_pic, xres-1, yres-1, xres/2, 0, &colors::RED)?;
+    test_pic.draw_line(0, 0, xres/2, yres-1, &colors::RED)?;
+    test_pic.draw_line(xres-1, yres-1, xres/2, 0, &colors::RED)?;
 
     // octants 7, 3
-    draw::line(&mut test_pic, 0, yres-1, xres/2, 0, &colors::MAGENTA)?;
-    draw::line(&mut test_pic, xres-1, 0, xres/2, yres-1, &colors::MAGENTA)?;
+    test_pic.draw_line(0, yres-1, xres/2, 0, &colors::MAGENTA)?;
+    test_pic.draw_line(xres-1, 0, xres/2, yres-1, &colors::MAGENTA)?;
 
     // horizontal and vertical
-    draw::line(&mut test_pic, 0, yres/2, xres-1, yres/2, &colors::YELLOW)?;
-    draw::line(&mut test_pic, xres/2, 0, xres/2, yres-1, &colors::YELLOW)?;
+    test_pic.draw_line(0, yres/2, xres-1, yres/2, &colors::YELLOW)?;
+    test_pic.draw_line(xres/2, 0, xres/2, yres-1, &colors::YELLOW)?;
 
     test_pic.save("test.ppm")?;
 
--- src/picture.rs
+++ src/picture.rs
@@ -2,8 +2,8 @@ use std::fs::File;
 use std::io::{self, Write};
 
 pub struct Picture {
-    xres: usize,
-    yres: usize,
+    pub xres: usize,
+    pub yres: usize,
     max_color: usize,
     data: Vec<Vec<(usize, usize, usize)>>,
 }
@@ -45,6 +45,59 @@ impl Picture {
         self.data[(self.yres - 1) - y][x] = (color.0, color.1, color.2);
 
         // everthing went well
+        Ok(())
+    }
+
+    pub fn draw_line(&mut self, mut x0: isize, mut y0: isize, x1: isize, y1: isize, color: &(usize, usize, usize)) -> io::Result<()> {
+        // using absolute value to make it case insensitive for the different octants
+        let dx = (x1 - x0).abs();
+        let dy = (y1 - y0).abs();
+
+        // if left to right then ++ otherwise --
+        let step_x = if x1 > x0 { 1 } else { -1 };
+        // if down to up then ++ otherwise --
+        let step_y = if y1 > y0 { 1 } else { -1 };
+        // ^^ these make it so 
+
+        // using dx and dy instead of y1 - y0 and x1 - x0 so it works for all octants
+        let a = 2 * dy;
+        let b = -2 * dx;
+
+        // 0 <= |m| <= 1
+        let small_slope = dy <= dx;
+
+        // if |m| > 1 then it's as if we're swapping x and y (reflection over y = x)
+        // a and b derived from line equation Ax + By + C
+        let mut d = if small_slope { a + b / 2 } else { a / 2 + b };
+
+        if small_slope {
+            // there is at least one pixel for every x value for small slope
+            while x0 != x1 {
+                self.plot(x0 as usize, y0 as usize, &color)?;
+
+                if d > 0 {
+                    y0 += step_y;
+                    d += b;
+                }
+
+                x0 += step_x;
+                d += a;
+            }
+        } else {
+            // there is at least one pixel for every y value for small slope
+            while y0 != y1 {
+                self.plot(x0 as usize, y0 as usize, &color)?;
+
+                if d < 0 {
+                    x0 += step_x;
+                    d += a;
+                }
+
+                y0 += step_y;
+                d += b;
+            }
+        }
+
         Ok(())
     }
 }
